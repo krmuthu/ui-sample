8fe5487fce57875838dbd6cbcbefcd7a77706a49
Muthukumar Kr <mk.kraam@gmail.com>
2025-03-14 18:42:06 +0800

    update next versiont

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Next.js 15 uses React 19 by default, so if we want to stay with React 18, specify it here
+  experimental: {
+    // Any Next.js 15 experimental features can be enabled here
+  }
 };
 
 export default nextConfig;
